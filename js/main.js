698109280f46f7306e023dedcac60aa07ab47b71
unknown <anwarhosen575@gmail.com>
2023-02-10 22:45:34 +0600

    second5

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-$('.one').hide(1000);
+$(document).ready(function(){
+    $('.text').click(function(){
+        $('.one').hide(1000);
+    });
+});
+$(document).ready(function(){
+    $('h3').click(function(){
+        $(this).hide(1000);
+    });
+});
