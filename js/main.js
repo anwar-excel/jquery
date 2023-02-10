2d18e3f7b74d9e3086426114199be507036f43a1
unknown <anwarhosen575@gmail.com>
2023-02-10 23:22:33 +0600

    second6

--- js/main.js
+++ js/main.js
@@ -8,3 +8,25 @@ $(document).ready(function(){
         $(this).hide(1000);
     });
 });
+$(document).ready(function(){
+    $('button').click(function(){
+        $('.third p.lorem1').hide(1000);
+    });
+});
+$(document).ready(function(){
+    $('button').click(function(){
+        $('.child p:first').hide(1000);
+    });
+});
+// ul first li delete
+// $(document).ready(function(){
+//     $('.delete').click(function(){
+//         $('ul li:first').hide(1000);
+//     });
+// });
+
+$(document).ready(function(){
+    $('.delete').click(function(){
+        $('ul li:first-child').hide(1000);
+    });
+});
